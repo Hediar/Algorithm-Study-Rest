55ee1e69158cd05d9a72f48987de714824f83daa
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-08-20 16:00:46 +0900

    16234_인구이동

--- UiseongSang/16234_인구이동.cpp
+++ UiseongSang/16234_인구이동.cpp
@@ -5,23 +5,29 @@
     알고리즘: DFS
     링크: https://www.acmicpc.net/problem/16234
 
+    이 문제는 인구이동이 일어나지 않을 때까지 평균값을 내고 각 지역마다
+    평균값이 된 인구를 집어 넣고 기초 입력값인 l이랑 r 범위에 벗어난 지역은 인구이동이 일어나지 않는다.
+    그래서 모든 지역이 인구이동이 일어나지 않을 때까지 인구이동은 진행이 되고 인구이동이 진행 될떄까지 cnt++를 해준다
+
+    DFS으로 탐색으로 풀었다! 그리고 매번 인구이동이 일어나면 모든 값을 초기화를 시캬주어 인구이동이 가능한지 check할 세팅을 해준다.
+    자료구조는 vector를 이용해 줘서 각 지역의 좌표와 사이즈와 sum을 알 수 있도록 해주었다!
 
 */ 
 #include <bits/stdc++.h>
 using namespace std;
-int n,l,r, a[54][54],sum = 0,cnt = 0;
-bool vis[54][54];
+int n,l,r, a[54][54],sum,cnt;
+int vis[54][54];
 vector<pair<int,int>> v;
 const int dy[4] = {-1,0,1,0};
 const int dx[4] = {0,1,0,-1};
 
 void dfs(int y, int x, vector<pair <int,int>> &v) {
     for(int i = 0; i < 4; i++) {
-        int ny = dy[i] + y;
-        int nx = dx[i] + x;
+        int ny = y + dy[i];
+        int nx = x + dx[i];
 
-        if(ny < 0 || ny >= n || nx < 0 || nx >= x || vis[ny][nx]) continue;
-        if(abs(a[ny][nx] - a[y][x]) >= l && abs(a[ny]nx] - a[y][x] <= r)) {
+        if(ny < 0 || ny >= n || nx < 0 || nx >= n || vis[ny][nx]) continue;
+        if(abs(a[ny][nx] - a[y][x]) >= l && abs(a[ny][nx] - a[y][x]) <= r) {
             vis[ny][nx] = 1;
             v.push_back({ny,nx});
             sum += a[ny][nx];
@@ -31,10 +37,10 @@ void dfs(int y, int x, vector<pair <int,int>> &v) {
 }
 
 int main() {
-    cin >> n >> l >> r1;
+    cin >> n >> l >> r;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            cin >> a[i]][j]
+            cin >> a[i][j];
         }
     }
     
@@ -51,7 +57,7 @@ int main() {
                     dfs(i,j,v);
                     if(v.size() == 1) continue;
                     for(pair<int,int> tmp : v) {
-                        a[tmp.first][b.second] = sum / v.size();
+                        a[tmp.first][tmp.second] = sum / v.size();
                         flag = 1;
                     }
                 }
@@ -60,7 +66,6 @@ int main() {
         if(!flag) break;
         cnt++;
     }
-
     cout << cnt;
     return 0;
 }
